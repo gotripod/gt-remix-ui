ee419422fad840e1bef899f3230d88992fabe6c2
Colin Ramsay <info@colinramsay.co.uk>
2024-06-05 14:44:10 +0100

    feat: upgrade

---
+++ vite.config.js
@@ -0,0 +1,55 @@
+import { defineConfig } from "vite";
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin,
+} from "@remix-run/dev";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    cloudflareDevProxyVitePlugin(),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+      routes: async (defineRoutes) => {
+        return defineRoutes((route) => {
+          route("/insights/page/:page", "routes/insights.tsx", {
+            id: 'insights-paged',
+            index: true
+          });
+          route("/insights/category/:category", "routes/insights.tsx", {
+            id: 'insights-category',
+            index: true
+          });
+          route("/insights/category/:category/page/:page", "routes/insights.tsx", {
+            id: 'insights-category-paged',
+            index: true
+          });
+          route("/insights/topic/:tag", "routes/insights.tsx", {
+            id: 'insights-tag',
+            index: true
+          });
+          route("/insights/topic/:tag/page/:page", "routes/insights.tsx", {
+            id: 'insights-tag-paged',
+            index: true
+          });
+        });
+      }
+    }),
+    tsconfigPaths(),
+  ],
+  ssr: {
+    resolve: {
+      conditions: ["workerd", "worker", "browser"],
+    },
+  },
+  resolve: {
+    mainFields: ["browser", "module", "main"],
+  },
+  build: {
+    minify: true,
+  },
+});
